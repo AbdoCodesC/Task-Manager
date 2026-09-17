1457658d7af7639c63bffd0936dbecaf1b0eb45c
AbdoCodesC <abdeerbusiness@gmail.com>
2026-09-17 10:50:24 -0400

    feat(schemas): validate backend request and response data

--- backend/schema/user_schema.py
+++ backend/schema/user_schema.py
@@ -1,7 +1,7 @@
 from app.extensions import ma
 from marshmallow.validate import Length, Regexp
 from marshmallow_sqlalchemy import SQLAlchemySchema
-from model import User
+from model.user import User, AccountStatus, UserRole
 # from schema import task_schema
 from db import db
 
@@ -10,20 +10,25 @@ class UserSchema(SQLAlchemySchema):
     model = User
     load_instance = True
     sqla_session = db.session
-    
+
   id = ma.UUID(dump_only=True)
   first_name = ma.String(required=True, validate=Length(min=3, error='First name must be at least 3 characters.'), error_messages={'required':'First name is required'})
   last_name = ma.String(required=True, validate=Length(min=3, error='Last name must be at least 3 characters.'), error_messages={'required':'Last name is required'})
   email = ma.Email(required=True, validate=Regexp(r"^\S+@\S+\.\S+$", error="Invalid email format"), error_messages={'required':'Email is required'})
   password = ma.String(required=True, validate=[Length(min=8, error='Password must be at least 8 characters.'), Regexp(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", error='Password must contain: 8+ characters, uppercase, lowercase, number, and special character (!@#$%^&* etc.)')], load_only=True, error_messages={'required':'Password is required'})
-  
+  account_status = ma.Enum(enum=AccountStatus, by_value=True, dump_only=True)
+  role = ma.Enum(enum=UserRole, by_value=True, dump_only=True)
+  email_verified = ma.Bool(dump_only=True)
+  token_version = ma.Integer(dump_only=True)
+
   created_at = ma.DateTime(dump_only=True)
   updated_at = ma.DateTime(dump_only=True)
-  
+  deleted_at = ma.DateTime(dump_only=True)
+
   owned_workspaces = ma.Nested("WorkspaceSchema", many=True, dump_only=True)
   workspace_memberships = ma.Nested("WorkspaceMemberSchema", many=True, dump_only=True)
   sent_invitations = ma.Nested("WorkspaceInvitationSchema", many=True, dump_only=True)
   activities = ma.Nested("TaskActivitySchema", many=True, dump_only=True)
-  
+
 user_schema = UserSchema()
 user_update_schema = UserSchema(partial=True)
